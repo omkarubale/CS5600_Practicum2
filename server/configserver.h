671bdd668dbdbc047e2a48f56d59d6a606fb44c6
Omkar Ubale <oubale@gmail.com>
2023-04-19 16:55:13 -0400

    CONFIG: updated usb directory for local usage for Omkar's PC

--- server/configserver.h
+++ server/configserver.h
@@ -4,6 +4,7 @@
 // Server configuration values
 
 // path of the root directory on server USB drive
-#define ROOT_DIRECTORY "/media/ujwal/UJWAL/root/"
+// #define ROOT_DIRECTORY "/media/ujwal/UJWAL/root/"
+#define ROOT_DIRECTORY "/Volumes/Omkar_PD/root/"
 
 #endif /* CONFIGSERVER_H */
